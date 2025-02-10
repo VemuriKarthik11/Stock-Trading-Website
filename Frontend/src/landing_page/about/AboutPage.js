fa1af3e0a7b833fabb77c3ed07b7f986dd687434
VemuriKarthik11 <vemurikarthik11@gmail.com>
2025-02-10 10:56:44 +0530

    Add files via upload

---
+++ Frontend/src/landing_page/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+import Team from "./Team";
+
+function AboutPage() {
+  return (
+    <>
+      <Home />
+      <Team />
+    </>
+  );
+}
+
+export default AboutPage;
